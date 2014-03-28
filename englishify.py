611cd04d127d0084c6e74831ec485a231c8d3a50
Dan Orr <danorr79@gmail.com>
2014-03-28 23:48:19 +1300

    completed first task of assignment, working on translating from pig_latin back to english

---
+++ englishify.py
@@ -0,0 +1,6 @@
+def englishify_sentence(sentence):
+    ''' Takes a pig latin sentence and translates it with variations '''
+    
+    words = sentence.split(" ")
+    
+def englishify_word(word):
--- pig_latin.py
+++ pig_latin.py
@@ -1,45 +1,50 @@
 def pig_latinify_sentence(sentence):
+    ''' Receives a sentence and returns an altered sentence '''
+    
+    # split the words and make a new list
     words = sentence.split(" ")
-    length = len(words)
+    # set up variables to test against or add to
     vowels = "aeiou"
     ending = ""
     outstring = ""
-    for i in range(len(words)): # what's the point of this?
-        for word in words:
-            if word[0] in vowels or word[0].isnumeric():
-                ending = "way"
-                if outstring:
-                    outstring = outstring + " " + (word + ending)
-                else:
-                    outstring = word + ending
-            #pig_latinify_word(word, ending)
+    #loop through list of words
+    for word in words:
+        # if word begins with vowel or is numeric set ending = "way"
+        if word[0] in vowels or word[0].isnumeric():
+            ending = "way"
+            if outstring:
+                outstring = outstring + " " + (word + ending)
             else:
-                ending = "ay"
-                if outstring:
-                    outstring = outstring + " " + (word[1:] + word[0].lower() + ending)
-                else:
-                    outstring = word[1:] + word[0].lower() + ending                    
-    return outstring
+                outstring = word + ending
+        else:
+            # word begins with a consonant, set ending = "ay"
+            ending = "ay"
+            if outstring:
+                outstring = outstring + " " + (word[1:] + word[0] + ending)
+            else:
+                outstring = word[1:] + word[0].lower() + ending                    
+    return outstring.strip().lower()
     
     
-def pig_latinify_word(word, ending):
-    ''' Receives word and vowel or consonant and
-        Returns the latinified word'''
-    if ending == "way":
-        build_string(word + ending)
-    else:
-        build_string(word[1:] + word[0].lower() + ending)
+#def pig_latinify_word(word, ending):
+    #''' Receives word and vowel or consonant and
+        #Returns the latinified word'''
+    #if ending == "way":
+        #build_string(word + ending)
+    #else:
+        #build_string(word[1:] + word[0].lower() + ending)
     
     
-def build_string(word):
-    outstring = ""
-    if not outstring == "":
-        outstring = outstring + " " + word
-    else:
-        outstring = word
-    return outstring
+#def build_string(word):
+    #outstring = ""
+    #if not outstring == "":
+        #outstring = outstring + " " + word
+    #else:
+        #outstring = word
+    #return outstring
 
    
 print(pig_latinify_sentence("Toby likes his art"))
 print(pig_latinify_sentence("Testing 123"))
 print(pig_latinify_sentence("Cook me some eggs"))
+
